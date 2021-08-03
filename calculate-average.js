9be6dd0a854c96e39b98c9c42f9be246d3558f3f
Austris <biosss91@gmail.com>
2021-08-03 21:32:46 +0300

    Create  calculate-average.js

---
+++ calculate-average.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/57a2013acf1fa5bfc4000921/train/javascript
+//Write a function which calculates the average of the numbers in a given list.
+//Note: Empty arrays should return 0.
+
+function find_average(array) {
+    const average = array.reduce((a, b) => a + b) / array.length;
+    return average;
+}
