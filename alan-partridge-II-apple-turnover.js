848b1ff6210b14087472801f28f1befe3ffb0c1d
Austris <biosss91@gmail.com>
2021-08-04 10:08:46 +0300

    Update alan-partridge-II-apple-turnover.js

--- alan-partridge-II-apple-turnover.js
+++ alan-partridge-II-apple-turnover.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
-//Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
+// Your job is simple, if (x) squared is more than 1000, return 'It's hotter than the sun!!', else, return 'Help yourself to a honeycomb Yorkie for the glovebox.'.
 // X will be a valid integer number.
 // X will be either a number or a string. Both are valid.
 
